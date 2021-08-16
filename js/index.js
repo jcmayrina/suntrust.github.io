32de20dee9a2f429045814bd4d6ae907fa0d664b
James Carlo C. Mayrina <jamescarlomayrina@gmail.com>
2021-08-16 21:48:15 +0800

    style and animation changes

--- js/index.js
+++ js/index.js
@@ -29,3 +29,34 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fix-nav");
   }
 });
+
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map((link) => {
+  if (!link) return;
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
+
+gsap.from(".logo1", { opacity: 0, duration: 0.5, delay: 0.2, y: -10 });
+gsap.from(".scroll-link", { opacity: 0, duration: 0.8, delay: 0.8, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
